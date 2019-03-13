cbc1a3f827f9ef3132187b7bc3a491ddc8f28093
diegosoriarios <diegosoriarios@gmail.com>
2019-03-13 12:43:29 -0300

    first commit

---
+++ app/src/main/java/com/example/diegorios/karcore/MainActivity.kt
@@ -0,0 +1,128 @@
+package com.example.diegorios.karcore
+
+import android.graphics.Point
+import android.net.Uri
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
+import com.google.ar.core.Anchor
+import com.google.ar.core.HitResult
+import com.google.ar.core.Plane
+import com.google.ar.core.TrackingState
+import com.google.ar.sceneform.AnchorNode
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.ux.ArFragment
+import com.google.ar.sceneform.ux.TransformableNode
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var arFragment: ArFragment
+
+    private var isTracking: Boolean = false
+    private var isHitting: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        arFragment.arSceneView.scene.addOnUpdateListener { frameTime ->
+            arFragment.onUpdate(frameTime)
+            onUpdate()
+        }
+
+        floatingActionButton.setOnClickListener {addObject(Uri.parse("NOVELO_EARTH.sfb"))}
+        showFab(false)
+    }
+
+    private fun showFab(enabled: Boolean) {
+        if(enabled) {
+            floatingActionButton.isEnabled = true
+            floatingActionButton.visibility = View.VISIBLE
+        } else {
+            floatingActionButton.isEnabled = false
+            floatingActionButton.visibility = View.GONE
+        }
+    }
+
+    private fun onUpdate() {
+        updateTracking()
+        if (isTracking) {
+            val hitTestChanged = updateHitTest()
+            if(hitTestChanged) {
+                showFab(isHitting)
+            }
+        }
+    }
+
+    private fun updateHitTest(): Boolean {
+        val frame = arFragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        val hits: List<HitResult>
+        val wasHitting = isHitting
+        isHitting = false
+        if (frame != null) {
+            hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for(hit in hits) {
+                val trackable = hit.trackable
+                if (trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    isHitting = true
+                    break
+                }
+            }
+        }
+        return wasHitting != isHitting
+    }
+
+    private fun updateTracking(): Boolean {
+        val frame = arFragment.arSceneView.arFrame
+        val wasTracking = isTracking
+        isTracking = frame.camera.trackingState == TrackingState.TRACKING
+        return isTracking != wasTracking
+    }
+
+    private fun getScreenCenter(): Point {
+        val view = findViewById<View>(android.R.id.content)
+        return Point(view.width / 2, view.height / 2)
+    }
+
+    private fun addObject(model: Uri) {
+        val frame = arFragment.arSceneView.arFrame
+        val point = getScreenCenter()
+        if (frame != null) {
+            val hits = frame.hitTest(point.x.toFloat(), point.y.toFloat())
+            for (hit in hits) {
+                var trackable = hit.trackable
+                if(trackable is Plane && trackable.isPoseInPolygon(hit.hitPose)) {
+                    placeObject(arFragment, hit.createAnchor(), model)
+                    break
+                }
+            }
+        }
+    }
+
+    private fun placeObject(fragment: ArFragment, anchor: Anchor, model: Uri) {
+        ModelRenderable.builder()
+            .setSource(fragment.context, model)
+            .build()
+            .thenAccept{
+                addNodeToScene(fragment, anchor, it)
+            }
+            .exceptionally {
+                Toast.makeText(this@MainActivity, "Error", Toast.LENGTH_SHORT).show()
+                return@exceptionally null
+            }
+    }
+
+    private fun addNodeToScene(fragment: ArFragment, anchor: Anchor, renderable: ModelRenderable) {
+        val anchorNode = AnchorNode(anchor)
+        val transformableNode = TransformableNode(fragment.transformationSystem)
+        transformableNode.renderable = renderable
+        transformableNode.setParent(anchorNode)
+        fragment.arSceneView.scene.addChild(anchorNode)
+        transformableNode.select()
+    }
+
+}
